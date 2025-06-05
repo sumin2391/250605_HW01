7899e877d8efcb5c89fc933c481e3b0bf415fd40
sumin2391 <baesumin0211@gmail.com>
2025-06-05 14:16:05 +0900

    수정1

--- 소스.cpp
+++ 소스.cpp
@@ -1,51 +1,36 @@
 #include <iostream>
 using namespace std;
 
-double 합계계산(double 배열[], int 크기) {
-    double 합 = 0;
-    for (int i = 0; i < 크기; i++) {
-        합 += 배열[i];
-    }
-    return 합;
-}
+int sum(int arr[], int 숫자)
+{
+    int s = 0;
+    for (int i = 0; i < 숫자; i++)
+    {
+        s += arr[i];
+    };
+    return s;
+};
 
-double 평균계산(double 배열[], int 크기) {
-    if (크기 == 0) return 0;
-    double 합 = 합계계산(배열, 크기);
-    return 합 / 크기;
-}
+int average(int arr[], int 숫자) 
+{
+    int s = 0;
+    for (int i = 0; i < 숫자; i++)
+    {
+        s += arr[i];
+    };
+    return s / 숫자;
+};
 
 int main() {
-    const int 개수 = 5;
-    double 숫자들[개수];
-
-    cout << "5개의 숫자를 입력하세요:" << endl;
-    for (int i = 0; i < 개수; i++) {
-        cout << (i + 1) << "번째 숫자: ";
-        cin >> 숫자들[i];
+    int nums[5];
+    for (int i = 0; i < 5; i++) {
+        std::cout << i+1 << "번째 숫자를 입력해주세요.";
+        std::cin >> nums[i];
     }
-
-    cout << "\n입력된 배열: [";
-    for (int i = 0; i < 개수; i++) {
-        cout << 숫자들[i];
-        if (i < 개수 - 1) cout << ", ";
-    }
-    cout << "]" << endl;
-
-    double 직접합계 = 0;
-    for (int i = 0; i < 개수; i++) {
-        직접합계 += 숫자들[i];
-    }
-    double 직접평균 = 직접합계 / 개수;
-
-    cout << "합계: " << 직접합계 << endl;
-    cout << "평균: " << 직접평균 << endl;
-
-    double 함수합계 = 합계계산(숫자들, 개수);
-    double 함수평균 = 평균계산(숫자들, 개수);
-
-    cout << "합계 함수 결과: " << 함수합계 << endl;
-    cout << "평균 함수 결과: " << 함수평균 << endl;
+    int s = sum(nums, 5);
+    double a = average(nums, 5);
+    std::cout << "합계: " << s << std::endl;
+    std::cout << "평균: " << a << std::endl;
 
     return 0;
 }
